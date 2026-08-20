49d2f64164348ec0537e6ce96e164a5081ad6a20
3667808244 <3667808244@qq.com>
2026-08-20 23:05:11 +0800

    ezmk 1.2.5 — update formula (macos-arm64 + linux-x64 digests)

--- Formula/ezmk.rb
+++ Formula/ezmk.rb
@@ -19,19 +19,19 @@
 class Ezmk < Formula
   desc "A simple C/C++ build tool (GCC/Clang/MSVC)"
   homepage "https://github.com/3667808244/EazyMake"
-  version "1.2.4"
+  version "1.2.5"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/3667808244/EazyMake/releases/download/v1.2.4/ezmk-macos-arm64.tar.gz"
-      sha256 "5f792efa22d624afaedffe71e14f921e4bec4278fecb26899431f7affac975e3"
+      url "https://github.com/3667808244/EazyMake/releases/download/v1.2.5/ezmk-macos-arm64.tar.gz"
+      sha256 "897fb5006d8225b810d27bdc798e4b85c1a5b38d9b91e9dffc59403a24c6877c"
     end
   end
 
   on_linux do
-    url "https://github.com/3667808244/EazyMake/releases/download/v1.2.4/ezmk-linux-x64.tar.gz"
-    sha256 "e19de2ee080a79e1f6b4c4055114c7d0dc18c28a04373a04a7b5bb531e2d4c2d"
+    url "https://github.com/3667808244/EazyMake/releases/download/v1.2.5/ezmk-linux-x64.tar.gz"
+    sha256 "c24bd0f65bc3d748debb9fc041c1fb25a426d70931781e29794a95dfb90a0abf"
   end
 
   def install
